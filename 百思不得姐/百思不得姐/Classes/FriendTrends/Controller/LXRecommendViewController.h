3cbec496174f5091a41f0714751bcef20788b26a
liuxing <liuxing@liuxingdemacbook-pro.local>
2016-09-20 01:51:50 +0800

    添加推荐关注类

---
+++ 百思不得姐/百思不得姐/Classes/FriendTrends/Controller/LXRecommendViewController.h
@@ -0,0 +1,13 @@
+//
+//  LXRecommendViewController.h
+//  百思不得姐
+//
+//  Created by liuxing on 16/9/20.
+//  Copyright © 2016年 kmznst. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LXRecommendViewController : UIViewController
+
+@end
